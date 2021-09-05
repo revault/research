4aba5f5d74ae44c66398f8dfdb003ebcc834ac3f
Antoine Poinsot <darosior@protonmail.com>
2021-09-05 16:09:19 +0200

    Take simultation parameters from env
    One step closer to cleaner configurability.. To get the same
    configuration as before under bash use:
    
    EXPECTED_ACTIVE_VAULTS=5 REFILL_EXCESS=$((3 * $EXPECTED_ACTIVE_VAULTS)) REFILL_PERIOD=$((144 * 7)) DELEGATION_PERIOD=144 INVALID_SPEND_RATE="0.1" CATASTROPHE_RATE="0.005" python3 main.py
    
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- Model/main.py
+++ Model/main.py
@@ -1,11 +1,18 @@
 import logging
 import os
 import random
+import sys
 
 from simulation import Simulation
 
 REPORT_FILENAME = os.getenv("REPORT_FILENAME", None)
 PLOT_FILENAME = os.getenv("PLOT_FILENAME", None)
+EXPECTED_ACTIVE_VAULTS = os.getenv("EXPECTED_ACTIVE_VAULTS", None)
+REFILL_EXCESS = os.getenv("REFILL_EXCESS", None)
+REFILL_PERIOD = os.getenv("REFILL_PERIOD", None)
+DELEGATION_PERIOD = os.getenv("DELEGATION_PERIOD", None)
+INVALID_SPEND_RATE = os.getenv("INVALID_SPEND_RATE", None)
+CATASTROPHE_RATE = os.getenv("CATASTROPHE_RATE", None)
 
 if __name__ == "__main__":
     random.seed(21000000)
@@ -28,8 +35,29 @@ if __name__ == "__main__":
     }
     logging.info(f"Using config {config}")
 
+    req_vars = [
+        EXPECTED_ACTIVE_VAULTS,
+        REFILL_EXCESS,
+        REFILL_PERIOD,
+        DELEGATION_PERIOD,
+        INVALID_SPEND_RATE,
+        CATASTROPHE_RATE,
+    ]
+    if any(v is None for v in req_vars):
+        logging.error(
+            "Need all these environment variables to be set: EXPECTED_ACTIVE_VAULTS,"
+            " REFILL_EXCESS, REFILL_PERIOD, DELEGATION_PERIOD, INVALID_SPEND_RATE,"
+            " CATASTROPHE_RATE."
+        )
+        sys.exit(1)
     sim = Simulation(
         config,
+        int(EXPECTED_ACTIVE_VAULTS),
+        int(REFILL_EXCESS),
+        int(REFILL_PERIOD),
+        int(DELEGATION_PERIOD),
+        float(INVALID_SPEND_RATE),
+        float(CATASTROPHE_RATE),
         with_balance=True,
         with_vault_excess=True,
         with_cum_op_cost=True,
