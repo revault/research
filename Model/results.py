418b27e8cd73fc8c43956d68113793c244ecce48
JSwambo <jacob@revault.dev>
2021-09-16 23:18:18 +0100

    fix risk_status, save results report_df as csv

--- Model/results.py
+++ Model/results.py
@@ -1,6 +1,7 @@
 import logging
 import multiprocessing as mp
 import os
+from pandas import DataFrame
 import pandas as pd
 import random
 import sys
@@ -10,7 +11,7 @@ from simulation import Simulation
 
 
 def sim_process(prng_seed, val=None, study_type=None, config_map=None):
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.ERROR)
     req_types = [
         "N_STK",
         "N_MAN",
@@ -81,7 +82,7 @@ def sim_process(prng_seed, val=None, study_type=None, config_map=None):
         with_balance=True,
         with_divergence=True,
         with_cum_op_cost=True,
-        with_overpayments=True,
+        with_risk_status=True,
     )
     try:
         sim.run(start_block, end_block)
@@ -118,24 +119,24 @@ if __name__ == "__main__":
     config_map = {
         "N_STK": 5,
         "N_MAN": 3,
-        "LOCKTIME": 12,
+        "LOCKTIME": 72,
         "HIST_CSV": "../block_fees/historical_fees.csv",
         "RESERVE_STRAT": "CUMMAX95Q90",
         "ESTIMATE_STRAT": "ME30",
-        "I_VERSION": 2,
-        "CANCEL_COIN_SELECTION": 0,
+        "I_VERSION": 3,
         "EXPECTED_ACTIVE_VAULTS": 5,
-        "REFILL_PERIOD": 144 * 7,
-        "REFILL_EXCESS": 5,
+        "REFILL_PERIOD": 144 * 31,
+        "REFILL_EXCESS": 1,
         "SPEND_RATE": 1,
+        "DELEGATE_RATE": 1,
         "INVALID_SPEND_RATE": 0.1,
         "CATASTROPHE_RATE": 0.005,
-        "DELEGATE_RATE": 1,
+        "CANCEL_COIN_SELECTION": 0,
     }
 
     # Set the study parameters
-    study_type = "REFILL_EXCESS"
-    val_range = [0, 0.5, 1, 1.5, 2]
+    study_type = "EXPECTED_ACTIVE_VAULTS"
+    val_range = [1, 5, 10, 25, 50, 100, 500]
     sim_repeats = 10
     cores = 10
 
@@ -143,6 +144,7 @@ if __name__ == "__main__":
     range_seed = list(range(21000000, 21000000 + cores))
 
     # Generate results
+    report_rows = []
     for val in val_range:
         config_map[study_type] = val
         report = (
@@ -162,9 +164,50 @@ if __name__ == "__main__":
         for df in sim_results:
             stats_df = pd.concat([stats_df, df], axis=0)
 
+        row = [val]
         for col in stats_df.columns:
             report += f"{col} mean:    {stats_df[col].mean()}\n"
             report += f"{col} std dev: {stats_df[col].std()}\n"
-
+            row.append(stats_df[col].mean())
+            row.append(stats_df[col].std())
+        report_rows.append(row)
         with open(f"{report_name}-{val}.txt", "w+", encoding="utf-8") as f:
             f.write(report)
+
+        # Save the csv at each val in case of failure
+        report_df = DataFrame(
+            report_rows,
+            columns=[
+                study_type,
+                "mean_balance_mean",
+                "mean_balance_std_dev",
+                "cum_ops_cost_mean",
+                "cum_ops_cost_std_dev",
+                "cum_cancel_fee_mean",
+                "cum_cancel_fee_std_dev",
+                "cum_cf_fee_mean",
+                "cum_cf_fee_std_dev",
+                "cum_refill_fee_mean",
+                "cum_refill_fee_std_dev",
+                "time_at_risk_mean",
+                "time_at_risk_std_dev",
+                "mean_recovery_time_mean",
+                "mean_recovery_time_std_dev",
+                "median_recovery_time_mean",
+                "median_recovery_time_std_dev",
+                "max_recovery_time_mean",
+                "max_recovery_time_std_dev",
+                "delegation_failure_count_mean",
+                "delegation_failure_count_std_dev",
+                "delegation_failure_rate_mean",
+                "delegation_failure_rate_std_dev",
+                "max_cancel_conf_time_mean",
+                "max_cancel_conf_time_std_dev",
+                "max_cf_conf_time_mean",
+                "max_cf_conf_time_std_dev",
+                "max_risk_coef_mean",
+                "max_risk_coef_std_dev",
+            ],
+        )
+        report_df.set_index(f"{study_type}", inplace=True)
+        report_df.to_csv(f"{report_name}")
